f7a47cd85f65f88bd43d6cd5107d4f3c2c6f35f2
thanhnt411 <darkphoenix4112006@gmail.com>
2025-10-02 20:04:31 +0700

    add migration

---
+++ database/migrations/2025_10_02_085110_create_trainings_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('trainings', function (Blueprint $table) {
+            $table->id();
+            $table->string('training_course');
+            $table->string('image');
+            $table->text('description');
+            $table->string('tuition_fee');
+            $table->string('study_mode');
+            $table->string('class_schedule');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('trainings');
+    }
+};
---
+++ database/migrations/2025_10_02_092843_create_settings_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('settings', function (Blueprint $table) {
+            $table->id();
+            $table->string('site_name');
+            $table->string('logo');
+            $table->string('email');
+            $table->string('phone');
+            $table->text('address');
+            $table->string('facebook_link');
+            $table->string('zalo_link');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('settings');
+    }
+};
